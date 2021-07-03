ae6cb110d23fa9c23812f61de96c9b5d5f68f133
ebrooker <18298060+ebrooker@users.noreply.github.com>
2021-07-02 21:33:05 -0400

    [3rd Day] Minor edit to hard_0003 challenge solution, fun little alternate version of challenge that returns not only the unscrambled words but any tail-truncated versions found in the main word list

--- src/hard_0003.py
+++ src/hard_0003.py
@@ -47,32 +47,46 @@ if __name__ == '__main__':
         wordlist = [line.strip() for line in fin.readlines()]
 
     # Method 1:
+    mstr='\n Method 1: '
     ti1 = perf_counter()
     hamlist     = [ ''.join(h for h in sorted(ham))  for ham  in wordlist                  ]
     scrambles   = [ ''.join(e for e in sorted(eggs)) for eggs in sorted(scrambled,key=len) ]
     unscrambled = [ ''.join(wordlist[hamlist.index(eggs)]) for eggs in scrambles           ]
     tf1 = perf_counter() - ti1
-    print("".join(f"{eggs}\n" for eggs in unscrambled))
+    mstr+="".join(f"{eggs} " for eggs in unscrambled)
+    print(mstr)
 
 
     # Method 2: (Adapted from method 2 to be a 1-liner, well, technically 2-liner including print statement)
+    mstr='\n Method 2: '
     ti2 = perf_counter()
     unscrambled = [ [ham for ham in wordlist if sorted(ham) == sorted(eggs)] for eggs in sorted(scrambled,key=len) ]
     tf2 = perf_counter()-ti2
-    print("".join(f"{eggs[0]}\n" for eggs in unscrambled))
+    mstr+="".join(f"{eggs[0]} " for eggs in unscrambled)
+    print(mstr)
 
 
     # Method 3: (Source: reddit user u/lnxaddct)
+    mstr = '\n Method 3: '
     ti3 = perf_counter()
     for eggs in sorted(scrambled, key=len):
         matches = [word for word in wordlist if sorted(eggs) == sorted(word)]
-        print(''.join(matches))
+        mstr+=''.join(matches)+' '
     tf3 = perf_counter() - ti3
+    print(mstr)
 
+    # Bonus Method 4: Use set() instead of sorted() to unscramble and find truncated versions of strings
+    mstr = '\n Method 4: '
+    ti4 = perf_counter()
+    unscrambled = [ [ham for ham in wordlist if set(eggs) == set(ham) ] for eggs in sorted(scrambled,key=len) ]
+    tf4 = perf_counter()-ti4
+    mstr+=" ".join(" ".join(egg for egg in eggs) for eggs in unscrambled)
+    print(mstr)
 
     print("\n#------- CPU Time -------#")
     print(f"#  Method 1: {tf1:2.7f}s  #")
     print(f"#  Method 2: {tf2:2.7f}s  #")
     print(f"#  Method 3: {tf3:2.7f}s  #")
+    print(f"#  Method 4: {tf4:2.7f}s  #")
     print("#------------------------#\n")
     
