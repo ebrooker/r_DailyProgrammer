b198bbeafe836ac454582f2e96bcd812789bc702
ebrooker <18298060+ebrooker@users.noreply.github.com>
2021-07-03 00:00:01 -0400

    [Day 4] Easy and intermediate challenges completed, random password generator and console calculator string expressions, respectively.

---
+++ src/easy_0004.py
@@ -0,0 +1,87 @@
+'''
+Ezra S. Brooker
+Date Created: 2021-07-02
+
+You're challenge for today is to create a random password generator!
+
+For extra credit, allow the user to specify the amount of passwords to generate.
+
+For even more extra credit, allow the user to specify the length of the strings
+they want to generate!
+
+'''
+
+import sys
+if sys.version[0] != '3':
+    raise Exception("Python 2 is no longer supported, please use Python 3")
+from random import sample
+from datetime import datetime
+
+class PasswordGenerator:
+
+    __ASCIIBET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=<>?,/;[]}{1234567890'
+    __LENASCII = len(__ASCIIBET)
+    
+
+    def __init__(self,npass=1,pwlen=16):
+
+        self.__npass = npass
+        self.__pwlen = pwlen
+
+
+    def clear(self):
+        self.__PASSWORDS = []
+
+
+    def set_asciibet(self,asciibet):
+        self.__ASCIIBET = asciibet        
+
+
+    def print_passwd(self):
+        [print(pw) for pw in self.__PASSWORDS]
+
+
+    def gen_passwd(self,npass=None, pwlen=None):
+
+        if npass is not None:
+            self.__npass = npass 
+        
+        if pwlen is not None:
+            self.__pwlen = pwlen
+
+        self.__PASSWORDS = {''.join(s for s in sample(self.__ASCIIBET,self.__pwlen)) : datetime.now() for _ in range(self.__npass)}
+
+
+    def save_passwd(self,file=None):
+
+        if file is not None:
+            self.__file = file
+        else:
+            self.__file = "../logs/easy_0004_passwd.log"
+
+        with open(self.__file, "a") as fout:
+            [fout.write(f"{v} : {k}\n") for k,v in self.__PASSWORDS.items()]
+
+
+if __name__ == '__main__':
+
+    # Generates, prints, and saves to file the number of and length of passwords specified
+    pw = PasswordGenerator()
+    pw.gen_passwd(
+            npass = int(input("Number of passwords to generate: ")),
+            pwlen = int(input("Length of passwords to generate: "))
+            ) 
+    pw.print_passwd()
+    pw.save_passwd()
+    pw.clear()
+    pw.print_passwd()
+
+    pw.set_asciibet("qwertyuioplkjhgfdsamnbvcxz0987654321")
+    pw.gen_passwd(
+            npass = int(input("Number of passwords to generate: ")),
+            pwlen = int(input("Length of passwords to generate: "))
+            ) 
+    pw.print_passwd()
+    pw.save_passwd()
+    pw.clear()
+    pw.print_passwd()
---
+++ src/intermediate_0004.py
@@ -0,0 +1,27 @@
+'''
+Ezra S. Brooker
+Date Created: 2021-07-02
+
+Create a calculator program that will take an input, following normal 
+calculator input (5*5+4) and give an answer (29). This calculator 
+should use all four operators.
+
+For extra credit, add other operators (6(4+3), 3 ** 3, etc.)
+
+'''
+
+import sys
+if sys.version[0] != '3':
+    raise Exception("Python 2 is no longer supported, please use Python 3")
+
+
+if __name__ == '__main__':
+
+    while True:
+        read = input("Enter calculation or <q> to quit: ")
+        if "q" in read.lower():
+            break
+        try:
+            print(eval(read))
+        except:
+            print("[ReadError] Invalid input")
